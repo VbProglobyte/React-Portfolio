e2ad6ddd776b25243740be5430ded43d19cbede9
Val Brisendine <vsbrisendine@gmail.com>
2022-01-13 12:52:52 -0600

    adding files and structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import './app.css';
+import reactPort from './components/reactPort';
+
+function App() {
+  return (
+    <div className="react-port">
+      <reactPort />
+    </div>
+  );
+}
+
+export default App;
